e8a7abf6cae68f6c0a8c5274399031dd6b000c25
Nadav <nadav.matalon@gmail.com>
2014-10-29 11:00:39 +0000

    fixed indentation

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,7 +2,7 @@ require 'fizzbuzz'
 
 describe 'Fizzbuzz' do 
 
-	it 'knows that a number is divisible by 3' do
+    it 'knows that a number is divisible by 3' do
 		expect(is_divisible_by_three?(3)).to be true
 	end
 
